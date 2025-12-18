9404635796a0fae2acc1c29267fc1ca6722a1706
GNAGNAN Jean Chrysostome <gjeanchrys@gmail.com>
2025-12-18 12:25:57 +0100

    Add Laravel application

--- start_ngrok.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Laravel + Ngrok Launcher
-echo 🚀 Lancement du serveur Laravel...
-start /B php artisan serve --host=127.0.0.1 --port=8000
-
-echo 🌍 Démarrage de Ngrok...
-timeout /t 3 >nul
-ngrok http 8000
-pause
